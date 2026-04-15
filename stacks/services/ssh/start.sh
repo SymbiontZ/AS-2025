2560e4777429741bca14ed52383812f1f7895f6f
Mariopeve <mariopeve5971@gmail.com>
2026-04-15 19:41:05 +0200

    SSH no ro

--- stacks/services/ssh/start.sh
+++ stacks/services/ssh/start.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-# Ensure strict permissions on authorized_keys before starting SSH daemon
-if [ -f "/home/admin_user/.ssh/authorized_keys" ]; then
-    chmod 600 /home/admin_user/.ssh/authorized_keys
+# Copy authorized_keys from the mounted read-only volume to the actual .ssh directory
+if [ -f "/tmp/authorized_keys" ]; then
+    cp /tmp/authorized_keys /home/admin_user/.ssh/authorized_keys
     chown admin_user:root /home/admin_user/.ssh/authorized_keys
+    chmod 600 /home/admin_user/.ssh/authorized_keys
 fi
 
 # Start ssh daemon in foreground
